2fa1086393adfbd48e7fbe92545fdb5532f1eb78
Andrei Kuleuski <kuljok@gmail.com>
2019-02-02 01:45:44 +0300

    Test for hellolib has been added

---
+++ tests/run_tests.sh
@@ -0,0 +1,29 @@
+echo "Build helloworld"
+cd ../00-helloworld
+(
+ rm -r build; mkdir build; cd build; cmake ..; cmake --build .;
+ ./helloworld > output.log;
+ if grep -q "Hello, World!" ./output.log;
+ then
+     echo "OK"
+ else
+     echo "ERROR"
+     exit 1
+ fi
+)
+cd ../01-hellolib/helloapp
+(
+    rm -r build;
+    mkdir build;
+    cd build;
+    cmake ..;
+    cmake --build .;
+    echo "Michael" | ./helloapp > output.log
+    if grep -q "Hello, Michael!" ./output.log;
+    then
+	echo "OK"
+    else
+	echo "ERROR"
+	exit 1
+    fi
+)
