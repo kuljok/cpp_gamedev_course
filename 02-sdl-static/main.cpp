7ae0ab9e16f002281baa3b9910aa143784cc0ed4
Andrei Kuleuski <kuljok@gmail.com>
2019-02-09 05:58:41 +0300

    HW 02-sdl-static has been added

---
+++ 02-sdl-static/main.cpp
@@ -0,0 +1,26 @@
+#include <SDL_version.h>
+#include <iostream>
+
+std::ostream& operator<<(std::ostream& o, const SDL_version& v)
+{
+    o << std::to_string(v.major) << '.' << std::to_string(v.minor) << '.'
+      << std::to_string(v.patch);
+    return o;
+}
+
+int main(int argc, char* argv[])
+{
+
+    using namespace std;
+
+    SDL_version compiled = { 0, 0, 0 };
+    SDL_version linked   = { 0, 0, 0 };
+
+    SDL_VERSION(&compiled);
+    SDL_GetVersion(&linked);
+
+    cout << "Compiled: " << compiled << endl;
+    cout << "Linked: " << linked << endl;
+
+    return cout.good() ? EXIT_SUCCESS : EXIT_FAILURE;
+}
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -40,7 +40,22 @@ then
     cmake --build .;
 )
 else
-    echo "Could not move to dir ../02-sdl-dynamic">&2
+    echo "Could not find the dir ../02-sdl-dynamic">&2
+    exit $?
+fi
+cd ../02-sdl-static
+if [ $? -eq 0 ]
+then
+    (
+	rm -r build;
+	mkdir build;
+	cd build;
+
+	cmake -DSDL2_INCLUDE_DIRS=/home/downloads/SDL2-2.0.9/include ..;
+	cmake --build .;
+    )
+else
+    echo "Could not find the dir ../02-sdl-static">&2
     exit $?
 fi
 
